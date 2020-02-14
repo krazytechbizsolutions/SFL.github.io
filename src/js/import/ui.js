7351643009d1678f8505b343ad8caf9a83907132
Tim <ktn_96@icloud.com>
2020-02-14 22:48:32 +0200

    add pop-up, help, training

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -1,18 +1,18 @@
 // import '../lib/maskedinput.js';
-// import modal from 'jquery-modal';
+import modal from 'jquery-modal';
 // import selectric from 'selectric';
 
 //select
 // $('select').selectric();
 
 
-// //pop-up
-// $('[rel="modal:open"]').on('click', function(event) {
-//   $(this).modal({
-//     fadeDuration: 200
-//   });
-//   return false;
-// });
+//pop-up
+$('[rel="modal:open"]').on('click', function(event) {
+  $(this).modal({
+    fadeDuration: 200
+  });
+  return false;
+});
 
 
 //home slide
